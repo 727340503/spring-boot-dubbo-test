6cb9af5477613c6761b342976c579659b68dfa1d
rahul <rahul@ACA80B3D.ipt.aol.com>
2018-01-22 17:38:45 +0800

    add web module

---
+++ tcc/tcc-cms/tcc-cms-web/src/main/java/com/cherrypicks/tcc/cms/api/Application.java
@@ -0,0 +1,26 @@
+package com.cherrypicks.tcc.cms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.cherrypicks.tcc.cms.api.config.WebConfig;
+
+@SpringBootApplication
+@EnableScheduling
+@Import({WebConfig.class})
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(final String[] args) {
+        final SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
+    }
+}
